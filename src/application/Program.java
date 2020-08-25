15d73965a7d97297481c87c5c0c1bef6c7deeaf7
Diego <diego.lima@icesp.org.br>
2020-08-25 18:26:59 -0300

    Atualização

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,65 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entitites.ImportedProduct;
+import entitites.Product;
+import entitites.UsedProduct;
+
 public class Program {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws ParseException {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		List<Product> list = new ArrayList<>();
+		
+		System.out.print("Enter the number of products: ");
+		int n = sc.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			System.out.println("Product #"+i+" data: ");
+			System.out.println("Common, used or imported (c/u/i)?");
+			char type = sc.next().charAt(0);
+			System.out.println("Name: ");
+			String name = sc.nextLine();
+			sc.nextLine();
+			System.out.println("Price: ");
+			double price = sc.nextDouble();
+			
+			if (type == 'c') {
+				
+				list.add(new Product(name, price));
+				
+			} else if (type == 'u'){
+				
+				System.out.print("Manufacture date (DD/MM/YYYY): ");
+				Date manufactureDate = sdf.parse(sc.next());
+				list.add(new UsedProduct(name, price, manufactureDate));
+				
+			} else if (type == 'i') {
+				
+				System.out.print("Customs fee: ");
+				double customsFee = sc.nextDouble();
+				list.add(new ImportedProduct(name, price, customsFee));
+				
+			}
+		}
+		
+		
+		
+		
+		
+		
+		sc.close();
 
 	}
 
--- src/entitites/UsedProduct.java
+++ src/entitites/UsedProduct.java
@@ -1,5 +1,43 @@
 package entitites;
 
-public class UsedProduct {
+import java.util.Date;
+
+public final class UsedProduct extends Product{
+	
+	private Date manufactureDate;
+	
+	private Product product = new Product();
+	
+	//metodo padrão
+	public UsedProduct() {
+		super();
+	}
+
+	//metodo com argumentos
+	public UsedProduct(String name, Double price, Date manufactureDate) {
+		super(name, price);
+		this.manufactureDate = manufactureDate;
+	}
+
+	//metodos GETTERS e SETTERS
+	public Date getManufactureDate() {
+		return manufactureDate;
+	}
+
+	public void setManufactureDate(Date manufactureDate) {
+		this.manufactureDate = manufactureDate;
+	}
+	
+	//Polimorfismos: utilização do método generico da superclasse e adicionando mais informações
+	@Override
+	public String priceTag() {
+		super.priceTag();
+		return product.getName()
+				+" $ "
+				+product.getPrice()
+				+" (Manufacture date: "
+				+manufactureDate
+				+")";
+	}
 
 }
